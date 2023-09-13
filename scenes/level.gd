fc3374bd2a8067ad84450e5549cdbfc4e34ac9b0
User <email@email.com>
2023-09-13 11:51:08 -0400

    refactor + pre-disaster

--- scenes/level.gd
+++ scenes/level.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	GameState.customize($Player, GameState.players[0][0], GameState.players[0][1], GameState.players[0][2], GameState.players[0][3])
+	GameState.customize($Player, GameState.players[0][0],
+		GameState.players[0][1], GameState.players[0][2], GameState.players[0][3])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
