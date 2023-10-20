81a752ef125f6017327846e46583d1c98e7788fc
User <email@email.com>
2023-10-20 06:02:10 -0400

    pre-pixel outline

--- plug.gd
+++ plug.gd
@@ -2,3 +2,4 @@ extends "res://addons/gd-plug/plug.gd"
 
 func _plugging():
 	plug("xxSeys1/Godot_ColoredColissionShapes", {"include": ["ColoredCollisionShapes/"]})
+	plug("KarlTheCool/FollowParent")
